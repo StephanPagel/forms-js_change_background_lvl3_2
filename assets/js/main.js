60645449f9be8a098ab05d8d65e2153f4b88731c
StephanPagel <stephan.pagel84@gmail.com>
2022-04-20 12:31:12 +0200

    cleaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,5 @@ let colorGreen = document.getElementById('color_green');
 let colorBlue = document.getElementById('color_blue');
 
 function changeBackground() {
-    console.log(colorRed.value, colorGreen.value, colorBlue.value);
     document.body.style.backgroundColor = 'rgb(' + colorRed.value + ',' + colorGreen.value + ',' + colorBlue.value + ') ';
 }
