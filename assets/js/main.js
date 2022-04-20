dc5060deb750e8cb9f2be290157742b7a3d49db0
StephanPagel <stephan.pagel84@gmail.com>
2022-04-20 12:29:30 +0200

    cleaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 let colorRed = document.getElementById('color_red');
 let colorGreen = document.getElementById('color_green');
 let colorBlue = document.getElementById('color_blue');
-// let body = document.getElementById('body');
 
 function changeBackground() {
     console.log(colorRed.value, colorGreen.value, colorBlue.value);
